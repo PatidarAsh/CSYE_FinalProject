acfa1c3030f5047974107a0db19841de734581ce
JasmnC <chen.jasm@northeastern.edu>
2023-04-18 15:56:44 -0400

    tutorial ui update

--- CSYE6200WordSearchGame/src/application/tutorial/GetTutorialStage.java
+++ CSYE6200WordSearchGame/src/application/tutorial/GetTutorialStage.java
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
 
 public class GetTutorialStage {
 
-	static	Label lbl1,lbl2,lbl3,lbl4,lbl5,lbl6,lbl7;
+	static	Label lbl1,lbl2,lbl3,lbl4,lbl5,lbl6,lbl7, lbl8;
 	static	FlowPane pane2;
 	static	Scene scene2;
 	static	Stage stage2;
@@ -28,14 +28,20 @@ public class GetTutorialStage {
 		
 		
 		lbl1 = new Label("Scene1");
-		lbl2 = new Label("Game Tutorial");
-		lbl3 = new Label();
-		lbl3.setText("To Play this game, you should follow this steps:");
-		lbl4 = new Label();
-		lbl4.setText("First, you should search your word.");
-		lbl5 = new Label("_____Use arrow keys to move (up/down/right/left)");
-		lbl6 = new Label("_____Enter to select each letter of the word.");
-		lbl7 = new Label("_____Use backspace to delete the most recent letter");
+		//lbl2 = new Label("Game Tutorial \n");
+		lbl3 = new Label("To Play this game, follow the steps:");
+		lbl3.setStyle("-fx-font:100px 'Apple Chancery'; -fx-fill: #418aa5; -fx-font-size: 14pt;");
+		lbl4 = new Label("    1) First, search for the word in puzzle.");
+		lbl4.setStyle("-fx-font:100px 'Apple Chancery'; -fx-fill: #418aa5; -fx-font-size: 12pt;");
+		lbl8 = new Label("    2) Then, select letters with keyboard.");
+		lbl8.setStyle("-fx-font:100px 'Apple Chancery'; -fx-fill: #418aa5; -fx-font-size: 12pt;");		
+		lbl5 = new Label("           - Use arrow keys to move (up/down/right/left)");
+		lbl5.setStyle("-fx-font:100px 'Bradley Hand'; -fx-fill: #418aa5; -fx-font-size: 10pt;");		
+		lbl6 = new Label("           - Enter to select each letter of the word.");
+		lbl6.setStyle("-fx-font:100px 'Bradley Hand'; -fx-fill: #418aa5; -fx-font-size: 10pt;");		
+		lbl7 = new Label("           - Use backspace to delete the most recent letter");
+		lbl7.setStyle("-fx-font:100px 'Bradley Hand'; -fx-fill: #418aa5; -fx-font-size: 10pt;");		
+
 		
 	
 		
@@ -44,9 +50,9 @@ public class GetTutorialStage {
 	    pane2.setVgap(10);
 	    
 	    pane2.setStyle("-fx-background-color:#c2d9d1;-fx-padding:10px;");
-	    pane2.getChildren().addAll(lbl2,lbl3,lbl4,lbl5,lbl6,lbl7);
+	    pane2.getChildren().addAll(lbl3,lbl4, lbl8,lbl5,lbl6,lbl7);
 	   
-	    scene2 = new Scene(pane2, 400, 200);
+	    scene2 = new Scene(pane2, 400, 250);
 	    scene2.setFill(Color.web("#708090"));
 	    stage2 = new Stage();
 	    stage2.setScene(scene2);
@@ -58,6 +64,7 @@ public class GetTutorialStage {
 	    
 	    
 		return stage2;
+		
 		
 	}
 
